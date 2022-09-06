fe8a45311f0cf24ef810ff8dcbb66ba9fddeb30c
Tanapat <tanapat.psr@mail.kmutt.ac.th>
2022-09-06 14:34:20 +0700

    getBMI

--- getBMIMeaning.test.js
+++ getBMIMeaning.test.js
@@ -1,14 +1,14 @@
 const testFn = require('./getBMIMeaning')
 
-test('output#1', () => {
+test('output#1', (calculateBMI) =>  {
   expect(testFn(65, 1.8)).toBe('Normal weight')
 })
 
-test('output#2', () => {
+test('output#2', (calculateBMI) => {
   expect(testFn(80, 1.7)).toBe('Overweight')
 })
 
-test('output#3', () => {
+test('output#3', (calculateBMI) => {
   expect(testFn(44, 1.6)).toBe('Underweight')
 })
 
