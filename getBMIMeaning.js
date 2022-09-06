48fad383f3286ed779d61233c7abcf2f2db7c2a5
Tanapat <tanapat.psr@mail.kmutt.ac.th>
2022-09-06 14:02:29 +0700

    calculate

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,14 @@
-const { template } = require('@babel/core')
+// const { template } = require('@babel/core')
+
+// let weight=75;
+// let height=172;
 
 function calculateBMI(weight, height) {
- return weight / ( ( height / 100 )* 2 );
+ return weight/((height / 100) ** 2) ;
 }
 function getBMIMeaning(weight, height) {
   return 
 }
+
+console.log(calculateBMI(70,170));
 module.exports = getBMIMeaning
