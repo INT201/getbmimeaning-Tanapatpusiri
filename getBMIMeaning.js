c89eb136ae1ff3b502b26cc727ab7a2b77bb60c2
Tanapat <tanapat.psr@mail.kmutt.ac.th>
2022-09-07 19:03:54 +0700

    test answer

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,15 +1,14 @@
-//. const { template } = require('@babel/core')
+  const { template } = require('@babel/core')
 
- //let weight=75;
- //let height=172;
+
 
 function calculateBMI(weight, height) {
  return weight/((height / 100) ** 2) ;
 }
 
 function getBMIMeaning(weight, height) {
-let BMI = weight/((height / 100) ** 2)
-
+ let BMI = weight/((height / 100) ** 2)
+ 
   if ( BMI<18.5){
     console.log(BMI)
     return("Normal weight")
@@ -24,11 +23,9 @@ let BMI = weight/((height / 100) ** 2)
   return("Underweight");
 }
 
-  
 
 
   
 
-console.log(calculateBMI(60,170));
-console.log(getBMIMeaning(60,170))
-module.exports = getBMIMeaning
+
+  module.exports = getBMIMeaning
--- getBMIMeaning.test.js
+++ getBMIMeaning.test.js
@@ -1,14 +1,14 @@
 const testFn = require('./getBMIMeaning')
 
-test('output#1', (calculateBMI) =>  {
+test('output#1', () =>  {
   expect(testFn(65, 1.8)).toBe('Normal weight')
 })
 
-test('output#2', (calculateBMI) => {
+test('output#2', () => {
   expect(testFn(80, 1.7)).toBe('Overweight')
 })
 
-test('output#3', (calculateBMI) => {
+test('output#3', () => {
   expect(testFn(44, 1.6)).toBe('Underweight')
 })
 
