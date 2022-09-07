344f1ea698b79ba0126ae67acfb9fd76d82e51ed
Tanapat <tanapat.psr@mail.kmutt.ac.th>
2022-09-07 19:24:45 +0700

    test answer3

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -11,16 +11,17 @@ function getBMIMeaning(weight, height) {
  
   if ( BMI<18.5){
     console.log(BMI)
-    return("Normal weight")
+    return("Underweight")
     
     
   } else if (BMI>=18.5) {
     console.log(BMI)
-    return("Overweight");
+    return("Normal weight");
 
   } else  (BMI>=25.0 )
   console.log(BMI)
-    return("Underweight");
+    return("Overweight");
 }
-
+console.log(calculateBMI(65,1.8))
+console.log(getBMIMeaning(65,1.8))
   module.exports = getBMIMeaning
